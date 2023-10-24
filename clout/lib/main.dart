6ff01bb65e18d365e8d85d5d3df508f8c155efe3
fctnm212 <fctnm212@naver.com>
2023-10-24 14:45:30 +0900

    Feat: style.dart

--- clout/lib/main.dart
+++ clout/lib/main.dart
@@ -1,14 +1,28 @@
 import 'package:flutter/material.dart';
+import 'style.dart' as style;
 
 void main() {
-  runApp(MaterialApp(home: MyApp()));
+  runApp(
+    
+    MaterialApp(
+      theme: ThemeData(
+        fontFamily: 'NotoSansKR',
+      ),
+      home: Scaffold(
+        backgroundColor: Colors.white, // 배경색을 흰색으로 변경
+        body: MyApp(),
+      ),));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('안녕');
+    return Text(
+      '안녕', // 텍스트 내용
+      style: style.textTheme.titleLarge?.copyWith(color: style.colors['main1']), // 스타일 설정 예시
+    );
   }
 }
+
