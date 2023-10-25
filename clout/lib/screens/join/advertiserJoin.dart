eb63ad849cfb455d7a1b084f321a920288683c7f
fctnm212 <fctnm212@naver.com>
2023-10-25 12:37:25 +0900

    Feat: clouterjoin, advertiserjoin route

---
+++ clout/lib/screens/join/advertiserJoin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AdvertiserJoin extends StatelessWidget {
+  const AdvertiserJoin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ clout/lib/screens/join/clouterJoin.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ClouterJoin extends StatelessWidget {
+  const ClouterJoin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
