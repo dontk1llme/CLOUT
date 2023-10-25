a301faad925b853d81e6b139cb8ef8a8cd3d4faf
Jaemin-Park9708 <catbirdseat9708@gmail.com>
2023-10-25 21:25:14 +0900

    S09P31A409-176 Feat: [FE] CampaignRegisterPage
    - Component 제작중 (80%)
    - Page 제작중 (70%)

--- clout/lib/screens/campaign_register/campaign_register.dart
+++ clout/lib/screens/campaign_register/campaign_register.dart
@@ -1,14 +1,16 @@
 // global
-import 'package:clout/screens/campaign_register/widgets/data_title.dart';
-import 'package:clout/screens/home/widgets/number_picker_dialog.dart';
+import 'package:clout/widgets/sns/sns3.dart';
 import 'package:flutter/material.dart';
 import 'package:clout/style.dart' as style;
 import 'package:get/get.dart';
 
+// Screens
+import 'package:clout/screens/campaign_register/widgets/data_title.dart';
+import 'package:clout/screens/home/widgets/number_picker_dialog.dart';
+
 // Widgets
 import 'package:clout/widgets/header/header.dart';
 import 'package:clout/widgets/input/input_elements/input_element.dart';
-import 'package:numberpicker/numberpicker.dart';
 
 class CampagignRegister extends StatefulWidget {
   CampagignRegister({super.key});
@@ -113,90 +115,112 @@ class _CampagignRegisterState extends State<CampagignRegister> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size.fromHeight(70),
-          child: Header(header: 4, headerTitle: '캠페인 작성')),
-      body: ListView(children: [
-        DataTitle(text: '카테고리'),
-        InputElement(
-          elementType: 'dropdown',
-          data: categories,
-          value: category,
-          placeholder: '--카테고리 선택--',
-          setData: setCategory,
-        ),
-        SizedBox(height: 10),
-        DataTitle(text: '제품명'),
-        SizedBox(
-            height: 80,
-            child: InputElement(
-              elementType: 'text',
-              placeholder: '제품명',
-              setData: setProductName,
-              maxLength: 50,
-            )),
-        SizedBox(height: 10),
-        DataTitle(text: '모집 인원(최대 100명)'),
-        SizedBox(
-            height: 50,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                TextButton(
-                    onPressed: () => showDialog(
-                        context: context,
-                        barrierDismissible: true,
-                        builder: ((context) {
-                          return NumberPickerDialog(
-                            value: recruitCount,
-                            minValue: 1,
-                            maxValue: 100,
-                            setData: setRecruitCount,
-                          );
-                        })),
-                    child: Text(
-                      recruitCount.toString(),
-                      style: style.textTheme.titleMedium?.copyWith(
-                          color: style.colors['main1'],
-                          fontWeight: FontWeight.bold,
-                          height: 1.1),
-                    )),
-                DataTitle(
-                  text: '명',
-                )
-              ],
-            )),
-        SizedBox(height: 10),
-        DataTitle(text: '게시 기간'),
-        SizedBox(height: 10),
-        Row(
-          children: [
-            DataTitle(text: '제공 내역'),
-          ],
-        ),
-        SizedBox(
-          height: 75,
-          child: InputElement(
-            elementType: 'text',
-            placeholder: '제공내역(제공할 물품 또는 서비스)',
-            setData: setOfferingItems,
-            maxLength: 300,
-          ),
-        ),
-        SizedBox(height: 10),
-        DataTitle(text: '제품 배송 여부'),
-        InputElement(
-          elementType: 'text',
-          placeholder: '제품 또는 서비스 상세 내용',
-          setData: setItemDetail,
-          maxLength: 500,
-          keyboardType: TextInputType.multiline,
-        ),
-        SizedBox(height: 10),
-        DataTitle(text: '제품 사진 첨부'),
-        DataTitle(text: '광고 희망 플랫폼'),
-        DataTitle(text: '희망 클라우터 정보'),
-      ]),
-    );
+        appBar: PreferredSize(
+            preferredSize: Size.fromHeight(70),
+            child: Header(header: 4, headerTitle: '캠페인 작성')),
+        body: SizedBox(
+            width: double.infinity,
+            child: ListView(children: [
+              FractionallySizedBox(
+                widthFactor: 0.9,
+                child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      DataTitle(text: '카테고리'),
+                      InputElement(
+                        elementType: 'dropdown',
+                        data: categories,
+                        value: category,
+                        placeholder: '--카테고리 선택--',
+                        setData: setCategory,
+                      ),
+                      SizedBox(height: 10),
+                      DataTitle(text: '제품명'),
+                      SizedBox(
+                          height: 55,
+                          child: InputElement(
+                            elementType: 'text',
+                            placeholder: '제품명',
+                            setData: setProductName,
+                            maxLength: 50,
+                          )),
+                      SizedBox(height: 10),
+                      DataTitle(text: '모집 인원(최대 100명)'),
+                      SizedBox(
+                          height: 50,
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              TextButton(
+                                  onPressed: () => showDialog(
+                                      context: context,
+                                      barrierDismissible: true,
+                                      builder: ((context) {
+                                        return NumberPickerDialog(
+                                          value: recruitCount,
+                                          minValue: 1,
+                                          maxValue: 100,
+                                          setData: setRecruitCount,
+                                        );
+                                      })),
+                                  child: Text(
+                                    recruitCount.toString(),
+                                    style: style.textTheme.titleMedium
+                                        ?.copyWith(
+                                            color: style.colors['main1'],
+                                            fontWeight: FontWeight.bold,
+                                            height: 1.1),
+                                  )),
+                              DataTitle(
+                                text: '명',
+                              )
+                            ],
+                          )),
+                      SizedBox(height: 10),
+                      DataTitle(text: '게시 기간'),
+                      SizedBox(height: 10),
+                      Row(
+                        children: [
+                          DataTitle(text: '제공 내역'),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 75,
+                        child: InputElement(
+                          elementType: 'text',
+                          placeholder: '제공내역(제공할 물품 또는 서비스)',
+                          setData: setOfferingItems,
+                          maxLength: 300,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      DataTitle(text: '제품 배송 여부'),
+                      InputElement(
+                        elementType: 'text',
+                        placeholder: '제품 또는 서비스 상세 내용',
+                        setData: setItemDetail,
+                        maxLength: 500,
+                        keyboardType: TextInputType.multiline,
+                      ),
+                      SizedBox(height: 10),
+                      DataTitle(text: '제품 사진 첨부'),
+                      DataTitle(text: '광고 희망 플랫폼'),
+                      Sns3(),
+                      DataTitle(text: '희망 최수 팔로워 수'),
+                      SizedBox(
+                        height: 55,
+                          child: InputElement(
+                        placeholder: '희망 최소 팔로워수(최대 1억)',
+                        elementType: 'text',
+                        setData: setMinimumFollowers,
+                        value: minimumFollowers,
+                        keyboardType: TextInputType.number,
+                        maxValue: 1000000000,
+                        maxLength: 20,
+                      )),
+                      DataTitle(text: '지역 선택')
+                    ]),
+              ),
+            ])));
   }
 }
