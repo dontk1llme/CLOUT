474a1f514309167c8481146c75fa60f638ce3181
boyoun1113 <boyoun1113@naver.com>
2023-10-25 22:07:20 +0900

    Feat: ChattingList 페이지 및 위젯 제작
    - chatting_list 페이지 제작
    - chatting_item_box 위젯 제작
    - like_button 컬러 수정
    - clouter_detail 완성

--- clout/lib/screens/chatting/chatting_list.dart
+++ clout/lib/screens/chatting/chatting_list.dart
@@ -1,4 +1,9 @@
+import 'package:clout/screens/chatting/widgets/chatting_item_box.dart';
 import 'package:flutter/material.dart';
+import 'package:clout/style.dart' as style;
+
+// widgets
+import 'package:clout/widgets/header/header.dart';
 
 class ChattingList extends StatefulWidget {
   const ChattingList({super.key});
@@ -10,6 +15,21 @@ class ChattingList extends StatefulWidget {
 class _ChattingListState extends State<ChattingList> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: style.colors['white'],
+      appBar: PreferredSize(
+          preferredSize: Size.fromHeight(70),
+          child: Header(
+            header: 1,
+            headerTitle: '채팅 목록', // 채널명 또는 계정명
+          )),
+      body: ListView(
+        children: [
+          ChattingItemBox(),
+          ChattingItemBox(),
+          ChattingItemBox(),
+        ],
+      ),
+    );
   }
 }
