28e6c46f52cd57c7637e59807b8abccd2c04b8f5
dontk1llme <letm2go@gmail.com>
2023-10-25 14:17:36 +0900

    Feat: 광고주 회원가입

---
+++ clout/lib/screens/join/numberVerify.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NumberVerify extends StatelessWidget {
+  const NumberVerify({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body:
+        Text('어쩌라고...'),
+    );
+  }
+}
