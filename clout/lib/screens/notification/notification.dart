e48dd0dc747ee8eef621b18e28e19c68a85488f7
Jaemin-Park9708 <catbirdseat9708@gmail.com>
2023-10-26 10:51:54 +0900

    S09P31A409-176 Feat: [FE] CampaignRegisterPage
    - bouncing_listview 구현

--- clout/lib/screens/notification/notification.dart
+++ clout/lib/screens/notification/notification.dart
@@ -1,3 +1,5 @@
+import 'package:clout/utilities/bouncing_listview.dart';
+import 'package:clout/utilities/my_scroll.dart';
 import 'package:flutter/material.dart';
 import '../../style.dart' as style;
 
@@ -23,27 +25,28 @@ class _NotificationPageState extends State<NotificationPage> {
               headerTitle: '알림 목록',
             )),
         body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            color: Color(0xffEBE7FF),
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(30),
-              topRight: Radius.circular(30),
+            width: double.infinity,
+            height: double.infinity,
+            decoration: BoxDecoration(
+              color: Color(0xffEBE7FF),
+              borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(30),
+                topRight: Radius.circular(30),
+              ),
             ),
-          ),
-          padding: EdgeInsets.only(top: 20),
-          child: ListView(
-            children: [
-              NotificationItem(),
-              NotificationItem(),
-              NotificationItem(),
-              NotificationItem(),
-              NotificationItem(),
-              NotificationItem(),
-              NotificationItem(),
-            ],
-          ),
-        ));
+            padding: EdgeInsets.only(top: 20),
+            child: BouncingListview(
+              child: Column(
+                children: [
+                  NotificationItem(),
+                  NotificationItem(),
+                  NotificationItem(),
+                  NotificationItem(),
+                  NotificationItem(),
+                  NotificationItem(),
+                  NotificationItem(),
+                ],
+              ),
+            )));
   }
 }
