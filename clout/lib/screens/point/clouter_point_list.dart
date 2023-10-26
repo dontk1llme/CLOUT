10f7f4ccbc5d228447a0c2542d7b1d3d8a6b89be
boyoun1113 <boyoun1113@naver.com>
2023-10-26 10:57:43 +0900

    Feat: MyWallet widget buttons 조건부 렌더링, Advertiser point list 페이지 제작
    - 내 지갑 버튼 clouter/advertiser 조건부 렌더링
    - 광고주 포인트 관리 페이지 제작

--- clout/lib/screens/point/clouter_point_list.dart
+++ clout/lib/screens/point/clouter_point_list.dart
@@ -1,10 +1,10 @@
-import 'package:clout/screens/point/widgets/point_item_box.dart';
 import 'package:flutter/material.dart';
 import 'package:clout/style.dart' as style;
 
 // widgets
 import 'package:clout/screens/point/widgets/my_wallet.dart';
 import 'package:clout/widgets/header/header.dart';
+import 'package:clout/screens/point/widgets/point_item_box.dart';
 
 class ClouterPointList extends StatefulWidget {
   const ClouterPointList({super.key});
@@ -17,29 +17,34 @@ class _ClouterPointListState extends State<ClouterPointList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: style.colors['white'],
-      appBar: PreferredSize(
-        preferredSize: Size.fromHeight(70),
-        child: Header(
-          header: 4,
-          headerTitle: '포인트 관리', // 채널명 또는 계정명
+        backgroundColor: style.colors['white'],
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(70),
+          child: Header(
+            header: 4,
+            headerTitle: '포인트 관리', // 채널명 또는 계정명
+          ),
         ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 20), // 왼쪽과 오른쪽 패딩을 10으로 설정
-        child: ListView(
-          children: [
-            MyWallet(),
-            Text('포인트 내역',
-                style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20)),
-            PointItemBox(),
-            PointItemBox(),
-            PointItemBox(),
-            PointItemBox(),
-            PointItemBox(),
-          ],
-        ),
-      ),
-    );
+        body: ListView(children: [
+          FractionallySizedBox(
+            widthFactor: 0.9,
+            child: Column(
+              children: [
+                MyWallet(userType: 'clouter'),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('포인트 내역',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 19)),
+                ),
+                PointItemBox(),
+                PointItemBox(),
+                PointItemBox(),
+                PointItemBox(),
+                PointItemBox(),
+              ],
+            ),
+          )
+        ]));
   }
 }
