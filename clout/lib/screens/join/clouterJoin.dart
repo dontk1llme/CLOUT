7ee6ac087057d76af63d782df0da9fbeec57caa0
dontk1llme <letm2go@gmail.com>
2023-10-26 14:06:39 +0900

    Feat: couterJoin 틀 완성
    Progress bar 정상 작동

--- clout/lib/screens/join/clouterJoin.dart
+++ clout/lib/screens/join/clouterJoin.dart
@@ -1,45 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:percent_indicator/linear_percent_indicator.dart';
 import 'package:get/get.dart';
+import 'package:clout/utilities/bouncing_listview.dart';
 
 import '../../style.dart' as style;
+
 import 'package:clout/screens/join/widgets/big_button.dart';
 import 'package:clout/screens/join/widgets/small_button.dart';
 import 'package:clout/screens/join/numberVerify.dart';
 
-class ClouterJoin extends StatelessWidget {
+import 'package:clout/screens/join/widgets/clouter/clouterJoin1.dart';
+import 'package:clout/screens/join/widgets/clouter/clouterJoin2.dart';
+import 'package:clout/screens/join/widgets/clouter/clouterJoin3.dart';
+
+class ClouterJoin extends StatefulWidget {
   const ClouterJoin({super.key});
 
   @override
+  _ClouterJoinState createState() => _ClouterJoinState();
+}
+
+class _ClouterJoinState extends State<ClouterJoin> {
+  int pageNum = 1;
+
+  void setPageNum(int newPageNum) {
+    setState(() {
+      pageNum = newPageNum;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    double percent = 0.3;
-
-
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.only(left:25, top:15),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            Text('가입하고', style: style.textTheme.titleMedium),
-            Row(
-              children: <Widget>[
-                Text('CLOUT', style: style.textTheme.titleMedium?.copyWith(
-                  color: style.colors['main1'])),
-                Text('와 함께', style: style.textTheme.titleMedium),
-              ],
-            ),
-            Text('매칭해요', style: style.textTheme.titleMedium),
-            LinearPercentIndicator(
-              padding: EdgeInsets.zero,
-              percent: percent,
-              lineHeight: 10,
-              backgroundColor: style.colors['category'],
-              progressColor: style.colors['logo'],
-              width: 320,
-            )
-          ],
+      body: BouncingListview(
+        child: Padding(
+          padding: EdgeInsets.only(left: 25, top: 15, right: 25, bottom: 15),
+          child: ListView(
+            shrinkWrap: true,
+            children: <Widget>[
+              Text('가입하고', style: style.textTheme.titleMedium),
+              Row(
+                children: <Widget>[
+                  Text('CLOUT', style: style.textTheme.titleMedium?.copyWith(
+                    color: style.colors['main1'])),
+                  Text('와 함께', style: style.textTheme.titleMedium),
+                ],
+              ),
+              Text('매칭해요', style: style.textTheme.titleMedium),
+              SizedBox(height: 10), // 20픽셀의 공백
+
+              //페이지별로 보여주기
+              if (pageNum == 1) // Page 1
+                ClouterJoin1(),
+              if (pageNum == 2) // Page 2
+                ClouterJoin2(),
+              if (pageNum == 3) // Page 2
+                ClouterJoin3(),
+              BigButton(
+                title: pageNum == 3 ? '완료' : '다음', // pageNum에 따라 버튼 텍스트 변경
+                function: () {
+                  // 페이지 번호 업데이트
+                  if (pageNum < 3) {
+                    setPageNum(pageNum + 1);
+                  }
+                }
+              ),
+            ],
+          ),
         ),
       ),
     );
