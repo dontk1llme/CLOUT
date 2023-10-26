89df1e503e7213af8cee8f84c04e9de27b1b82da
boyoun1113 <boyoun1113@naver.com>
2023-10-26 22:27:05 +0900

    Feat: 검색 조건 모달 제작, Input 위젯 수정
    - search_detail_button 검색 조건 모달 제작
    - Input 위젯 padding 삭제
    - data_title_thin 위젯 제작

--- clout/lib/widgets/buttons/search_detail_button.dart
+++ clout/lib/widgets/buttons/search_detail_button.dart
@@ -1,16 +1,62 @@
-import 'package:clout/screens/campaign_register/widgets/age_slider.dart';
-import 'package:clout/widgets/input/input.dart';
-import 'package:clout/widgets/input/input_elements/widgets/dropdown_input.dart';
-import 'package:clout/widgets/sns/sns3.dart';
 import 'package:flutter/material.dart';
-import 'package:syncfusion_flutter_sliders/sliders.dart';
-import '../../style.dart' as style;
+import 'package:clout/style.dart' as style;
 
-class SearchDetailButton extends StatelessWidget {
+// widgets
+import 'package:clout/screens/campaign_register/widgets/minimumfollowers_dialog.dart';
+import 'package:clout/widgets/sns/sns3.dart';
+import 'package:clout/screens/campaign_register/widgets/age_slider.dart';
+import 'package:clout/screens/campaign_register/widgets/region_multiselect.dart';
+import 'package:clout/widgets/buttons/big_button.dart';
+import 'package:clout/widgets/input/input.dart';
+
+// Screens
+import 'package:clout/widgets/list/data_title_thin.dart';
+
+class SearchDetailButton extends StatefulWidget {
   SearchDetailButton({super.key, this.setAge, this.ageRanges});
-
   var setAge;
   var ageRanges;
+
+  @override
+  State<SearchDetailButton> createState() => _SearchDetailButtonState();
+}
+
+class _SearchDetailButtonState extends State<SearchDetailButton> {
+  var maxFee;
+  var minFee;
+  var minimumFollowers;
+  var minimumFollowersString = '0';
+  var selectedRegions = [];
+
+  setMaxFee(input) {
+    setState(() {
+      maxFee = input;
+    });
+  }
+
+  setMinFee(input) {
+    setState(() {
+      minFee = input;
+    });
+  }
+
+  setSelectedRegions(input) {
+    setState(() {
+      selectedRegions = input;
+    });
+  }
+
+  setMinimumFollowers(input) {
+    setState(() {
+      minimumFollowers = input;
+    });
+  }
+
+  setMinimumFollowersString(input) {
+    setState(() {
+      minimumFollowersString = input;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,38 +74,88 @@ class SearchDetailButton extends StatelessWidget {
           onTap: () {
             showModalBottomSheet<void>(
               context: context,
+              isScrollControlled: true,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
               builder: (BuildContext context) {
                 return Container(
-                  // color: Colors.black,
-                  height: 600,
-                  child: Center(
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisSize: MainAxisSize.min,
-                      children: <Widget>[
-                        ElevatedButton(
-                          child: const Text('Close BottomSheet'),
-                          onPressed: () => Navigator.pop(context),
+                  padding: EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+                  // child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            Text('상세 조건 설정',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.w600,
+                                  fontSize: 22,
+                                )),
+                            InkWell(
+                              child: Icon(
+                                Icons.close,
+                              ),
+                              onTap: () {
+                                Navigator.pop(context);
+                              },
+                            )
+                          ]),
+                      DataTitleThin(text: '희망 광고 플랫폼', pdtop: 20),
+                      Sns3(),
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: Text('다중 선택 가능',
+                            style: TextStyle(color: style.colors['gray'])),
+                      ),
+                      DataTitleThin(text: '희망 클라우터 나이', pdtop: 10),
+                      // slider 추가
+                      AgeSlider(
+                          setAge: widget.setAge, ageRanges: widget.ageRanges),
+                      // dropdown 추가
+                      DataTitleThin(text: '희망 최소 팔로워 수', pdtop: 25),
+                      MinimumfollowersDialog(
+                          minimumFollowers: minimumFollowers,
+                          minimumFollowersString: minimumFollowersString,
+                          setMinimumFollowers: setMinimumFollowers,
+                          setMinimumFollowersString: setMinimumFollowersString),
+                      DataTitleThin(text: '희망 광고비', pdtop: 10),
+                      Row(
+                        children: [
+                          Flexible(
+                            flex: 1,
+                            child: Input(
+                              placeholder: '최소 금액 (만원)',
+                              setText: setMinFee,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Flexible(
+                            flex: 1,
+                            child: Input(
+                              placeholder: '최대 금액 (만원)',
+                              setText: setMaxFee,
+                            ),
+                          ),
+                        ],
+                      ),
+                      DataTitleThin(text: '지역 선택', pdtop: 20),
+                      RegionMultiSelect(
+                          selectedRegions: selectedRegions,
+                          setSelectedRegions: setSelectedRegions),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: BigButton(
+                          title: '검색',
+                          destination: '/campaignlist',
                         ),
-                        Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('상세 조건 설정'),
-                              Icon(Icons.close),
-                            ]),
-                        Text('광고 희망 플랫폼'),
-                        Sns3(),
-                        Align(
-                          alignment: Alignment.centerRight,
-                          child: Text('다중 선택 가능'),
-                        ),
-                        Text('희망 클라우터 나이'),
-                        // slider 추가
-                        AgeSlider(setAge: setAge, ageRanges: ageRanges),
-                        // dropdown 추가
-                      ],
-                    ),
+                      )
+                    ],
+                    // ),
                   ),
                 );
               },
