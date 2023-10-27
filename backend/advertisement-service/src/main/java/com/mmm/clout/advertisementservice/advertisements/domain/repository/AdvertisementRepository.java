aab36eea08afe0ad777d1fa89d86a0ab90efd66e
윤다정 <ydajeong7@gmail.com>
2023-10-27 12:50:39 +0900

    Chore: 광고 스프링 서버 폴더 삭제

--- backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/advertisements/domain/repository/AdvertisementRepository.java
+++
@@ -1,8 +0,0 @@
-package com.mmm.clout.advertisementservice.advertisements.domain.repository;
-
-import com.mmm.clout.advertisementservice.advertisements.domain.Campaign;
-
-public interface AdvertisementRepository {
-
-    Campaign save(Campaign campaign);
-}
