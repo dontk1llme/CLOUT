a97ff78fbf8f22b45aa5650f7ea0af2b61b165e9
정승구 <jsggo2001@naver.com>
2023-10-27 17:00:45 +0900

    Feat: [BE] value 파일들 생성자 및 변수 수정

--- backend/user-service/src/main/java/com/mmm/clout/userservice/advertiser/domain/CompanyInfo.java
+++ backend/user-service/src/main/java/com/mmm/clout/userservice/advertiser/domain/CompanyInfo.java
@@ -1,8 +1,16 @@
 package com.mmm.clout.userservice.advertiser.domain;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
 
 @Embeddable
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CompanyInfo {
 
     private String companyName;
