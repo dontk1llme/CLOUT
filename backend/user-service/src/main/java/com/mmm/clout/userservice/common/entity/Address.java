d3fa97f0925eedc62bede356852220c28b565fd0
정승구 <jsggo2001@naver.com>
2023-10-27 16:58:50 +0900

    Feat: [BE] 주소 equals와 hashcode

--- backend/user-service/src/main/java/com/mmm/clout/userservice/common/entity/Address.java
+++ backend/user-service/src/main/java/com/mmm/clout/userservice/common/entity/Address.java
@@ -1,8 +1,17 @@
 package com.mmm.clout.userservice.common.entity;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Embeddable;
+import java.util.Objects;
 
 @Embeddable
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
 public class Address {
 
     private String zipCode;
@@ -11,4 +20,16 @@ public class Address {
 
     private String detailAddress;
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(zipCode, address.zipCode) && Objects.equals(mainAddress, address.mainAddress) && Objects.equals(detailAddress, address.detailAddress);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(zipCode, mainAddress, detailAddress);
+    }
 }
