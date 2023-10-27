84a2a19edb06ddd89504d15fe1d5a2a725e42a79
정승구 <jsggo2001@naver.com>
2023-10-27 13:49:36 +0900

    Feat:[BE] 유저 마이크로서비스 생성

---
+++ backend/user-service/src/main/java/com/mmm/clout/userservice/common/entity/Category.java
@@ -0,0 +1,5 @@
+package com.mmm.clout.userservice.common.entity;
+
+public enum Category {
+    FASHOIN_BEAUTY, HEALTH, TRAVEL, LIVING, PARENTING, TECH, FOOD, EDUCATION, SPORT_DANCE, PET, ENTERTAINMENT, CAR, GAME, ECONOMY, ETC
+}
