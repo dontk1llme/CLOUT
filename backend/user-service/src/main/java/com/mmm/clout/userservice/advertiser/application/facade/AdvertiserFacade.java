8530b7fe95829e6aa248c3a9dbc5c1bb579abbde
정승구 <jsggo2001@naver.com>
2023-10-29 18:18:23 +0900

    Feat: [BE] 광고주 조회 로직 구현

--- backend/user-service/src/main/java/com/mmm/clout/userservice/advertiser/application/facade/AdvertiserFacade.java
+++ backend/user-service/src/main/java/com/mmm/clout/userservice/advertiser/application/facade/AdvertiserFacade.java
@@ -1,6 +1,7 @@
 package com.mmm.clout.userservice.advertiser.application.facade;
 
 import com.mmm.clout.userservice.advertiser.application.CreateAdvertiserProcessor;
+import com.mmm.clout.userservice.advertiser.application.SelectAdvertiserProcessor;
 import com.mmm.clout.userservice.advertiser.application.UpdateAdvertiserProcessor;
 import com.mmm.clout.userservice.advertiser.application.command.CreateAdrCommand;
 import com.mmm.clout.userservice.advertiser.application.command.UpdateAdrCommand;
@@ -16,10 +17,14 @@ public class AdvertiserFacade {
 
     private final CreateAdvertiserProcessor createAdvertiserProcessor;
     private final UpdateAdvertiserProcessor updateAdvertiserProcessor;
+    private final SelectAdvertiserProcessor selectAdvertiserProcessor;
 
     public Advertiser create(CreateAdrCommand command) {
         return createAdvertiserProcessor.excute(command);
     }
 
     public Advertiser update(UpdateAdrCommand command) {return updateAdvertiserProcessor.excute(command); }
+
+    public Advertiser select(Long advertiserId) { return selectAdvertiserProcessor.excute(advertiserId);
+    }
 }
