25dd5b231e33f693afba829157e36adb360b069e
정승구 <jsggo2001@naver.com>
2023-10-30 10:02:36 +0900

    Refactor: [BE] advertiser 중복 에러 처리 리펙토링

---
+++ backend/user-service/src/main/java/com/mmm/clout/userservice/advertiser/infrastructure/exceptuion/AdrIdDuplicateException.java
@@ -0,0 +1,15 @@
+package com.mmm.clout.userservice.advertiser.infrastructure.exceptuion;
+
+import com.mmm.clout.userservice.common.exception.CustomBaseException;
+import com.mmm.clout.userservice.common.exception.ErrorCode;
+
+public class AdrIdDuplicateException extends CustomBaseException {
+
+    public AdrIdDuplicateException(String message, ErrorCode errorCode) {
+        super(message, errorCode);
+    }
+
+    public AdrIdDuplicateException(ErrorCode errorCode) {
+        super(errorCode);
+    }
+}
