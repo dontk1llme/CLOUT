7f0ca326e010cab0c635bf672faa303d3f43ac30
boyoun1113 <boyoun1113@naver.com>
2023-10-30 16:28:19 +0900

    Feat: 광고주 포인트 충전 안내모달 제작
    - 광고주 포인트 충전 시 안내 모달 팝업 제작

--- clout/lib/screens/point/widgets/my_wallet.dart
+++ clout/lib/screens/point/widgets/my_wallet.dart
@@ -1,3 +1,6 @@
+import 'package:clout/screens/point/withdraw/widgets/bold_text.dart';
+import 'package:clout/screens/point/withdraw/widgets/medium_text.dart';
+import 'package:clout/widgets/buttons/big_button.dart';
 import 'package:flutter/material.dart';
 import 'package:clout/style.dart' as style;
 import 'package:get/get.dart';
@@ -5,15 +8,124 @@ import 'package:get/get.dart';
 // widgets
 import 'package:clout/widgets/buttons/small_button.dart';
 
-class MyWallet extends StatelessWidget {
+class MyWallet extends StatefulWidget {
   final String userType; // clouter 인지 advertiser 인지
 
   const MyWallet({super.key, required this.userType});
 
   @override
+  State<MyWallet> createState() => _MyWalletState();
+}
+
+class _MyWalletState extends State<MyWallet> {
+  void _showModal() {
+    showModalBottomSheet<void>(
+      context: context,
+      isScrollControlled: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(20.0),
+      ),
+      builder: (BuildContext context) {
+        return Container(
+          padding: EdgeInsets.symmetric(vertical: 30, horizontal: 40),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  InkWell(
+                    child: Icon(
+                      Icons.close,
+                    ),
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  )
+                ],
+              ),
+              Icon(
+                Icons.warning,
+                size: 30,
+                color: Colors.amber,
+              ),
+              SizedBox(height: 15),
+              MediumText(text: '포인트 충전 후'),
+              MediumText(text: '보유한 포인트는'),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  MediumText(text: '현금으로'),
+                  SizedBox(width: 5),
+                  BoldText(text: '재환전이'),
+                  SizedBox(width: 3),
+                  Text('불가',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                          color: Colors.red)),
+                  MediumText(text: '합니다.')
+                ],
+              ),
+              SizedBox(height: 15),
+              Text('포인트는 사용할 만큼만'),
+              Text('충전하여 사용해주세요!'),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 25),
+                child: SizedBox(
+                  height: 30,
+                  width: 230,
+                  child: DecoratedBox(
+                    decoration: BoxDecoration(
+                      color: style.colors['category'],
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center, // 가운데 정렬
+                      children: [
+                        Icon(Icons.check, color: style.colors['main1']),
+                        SizedBox(width: 3),
+                        Text('내용을 확인했습니다.'),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: BigButton(
+                        title: '아니요',
+                        textColor: Colors.black,
+                        buttonColor: style.colors['lightgray'],
+                        function: () {
+                          Navigator.of(context).pop();
+                        }),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Flexible(
+                      flex: 1,
+                      child: BigButton(
+                        title: '충전하기',
+                      )),
+                ],
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     Widget buttons;
-    if (userType == 'advertiser') {
+    if (widget.userType == 'advertiser') {
       buttons = Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
@@ -21,21 +133,21 @@ class MyWallet extends StatelessWidget {
             flex: 1,
             child: SmallButton(
               title: '충전하기',
-              destination: 'withdraw',
-              function: () {},
+              // destination: 'chargemessage',
+              function: _showModal,
             ),
           ),
-          SizedBox(width: 5),
-          Flexible(
-            flex: 1,
-            child: SmallButton(
-              title: '출금하기',
-              destination: 'withdrawfirst',
-            ),
-          ),
+          // SizedBox(width: 5),
+          // Flexible(
+          //   flex: 1,
+          //   child: SmallButton(
+          //     title: '출금하기',
+          //     destination: 'withdrawfirst',
+          //   ),
+          // ),
         ],
       );
-    } else if (userType == 'clouter') {
+    } else if (widget.userType == 'clouter') {
       buttons = Flexible(
         flex: 1,
         child: SmallButton(
