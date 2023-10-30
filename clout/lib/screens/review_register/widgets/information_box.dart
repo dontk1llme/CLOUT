b0b15d63c2b4e7bbb7aba127c64e431499eec927
boyoun1113 <boyoun1113@naver.com>
2023-10-30 17:02:59 +0900

    Fix: Widgets 반응형으로 수정, mypage list들 padding 값 수정

--- clout/lib/screens/review_register/widgets/information_box.dart
+++ clout/lib/screens/review_register/widgets/information_box.dart
@@ -6,51 +6,66 @@ class InformationBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    final containerWidth = screenWidth - 40;
+    final imageSize = screenWidth * 0.2;
+    final textSpacing = screenWidth * 0.0375;
+    final fontSize1 = screenWidth * 0.046;
+    final fontSize2 = screenWidth * 0.03;
+    final fontSize3 = screenWidth * 0.045;
+
     return Container(
-        height: 150,
-        padding: EdgeInsets.all(20),
-        margin: EdgeInsets.fromLTRB(20, 35, 20, 30),
-        decoration: BoxDecoration(
-          color: style.colors['white'],
-          borderRadius: BorderRadius.circular(10),
-          boxShadow: style.shadows['shadow'],
-        ),
-        child: Row(
-          children: [
-            ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: Image.asset(
-                'assets/images/clouterImage.jpg',
-                width: 120,
-                height: 120,
-                fit: BoxFit.cover,
+      width: containerWidth,
+      padding: EdgeInsets.all(20),
+      margin: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+      decoration: BoxDecoration(
+        color: style.colors['white'],
+        borderRadius: BorderRadius.circular(10),
+        boxShadow: style.shadows['shadow'],
+      ),
+      child: Row(
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(10),
+            child: Image.asset(
+              'assets/images/clouterImage.jpg',
+              width: imageSize,
+              height: imageSize,
+              fit: BoxFit.cover,
+            ),
+          ),
+          SizedBox(
+            width: textSpacing,
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                '모카우유',
+                style: TextStyle(
+                  fontWeight: FontWeight.w700,
+                  fontSize: fontSize1,
+                ),
               ),
-            ),
-            SizedBox(
-              width: 20, // 이미지와 텍스트 사이의 간격 설정
-            ),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('모카우유',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20,
-                    )),
-                Text('2023.10.01 ~ 2023.10.25',
-                    style: TextStyle(
-                      fontSize: 15,
-                    )),
-                Text('80만 포인트',
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 18,
-                      color: style.colors['main1'],
-                    )),
-              ],
-            )
-          ],
-        ));
+              Text(
+                '2023.10.01 ~ 2023.10.25',
+                style: TextStyle(
+                  fontSize: fontSize2,
+                ),
+              ),
+              Text(
+                '80만 포인트',
+                style: TextStyle(
+                  fontWeight: FontWeight.w600,
+                  fontSize: fontSize3,
+                  color: style.colors['main1'],
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
--- clout/lib/widgets/common/nametag.dart
+++ clout/lib/widgets/common/nametag.dart
@@ -9,30 +9,32 @@ class NameTag extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final screenWidth = MediaQuery.of(context).size.width;
-    final categoryNameWidth = screenWidth > 400 ? 70.0 : 55.0;
-    final categoryNameHeight = screenWidth > 400 ? 30.0 : 20.0;
+    final categoryNameWidth = screenWidth * 0.175;
+    final categoryNameHeight = screenWidth * 0.05; 
 
     return Padding(
-        padding: EdgeInsets.only(top: 5, right: 4),
-        child: SizedBox(
-          width: categoryNameWidth,
-          height: categoryNameHeight,
-          child: DecoratedBox(
-            decoration: BoxDecoration(
-              color: style.colors['category'],
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: Center(
-              child: Text(
-                title,
-                style: TextStyle(
-                  color: style.colors['white'],
-                  fontWeight: FontWeight.w700,
-                  fontSize: categoryNameHeight - 10,
-                ),
+      padding: EdgeInsets.only(top: 5, right: 4),
+      child: SizedBox(
+        width: categoryNameWidth,
+        height: categoryNameHeight,
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: style.colors['category'],
+            borderRadius:
+                BorderRadius.circular(screenWidth * 0.01),
+          ),
+          child: Center(
+            child: Text(
+              title,
+              style: TextStyle(
+                color: style.colors['white'],
+                fontWeight: FontWeight.w700,
+                fontSize: categoryNameHeight * 0.6,
               ),
             ),
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
