dc4777dfb7dce1540deb8ccabb39373508815252
fctnm212 <fctnm212@naver.com>
2023-10-31 09:34:45 +0900

    Fix: 계약서 라우터 버튼 작동하도록

--- clout/lib/screens/mypage/widgets/small_contract.dart
+++ clout/lib/screens/mypage/widgets/small_contract.dart
@@ -1,4 +1,5 @@
 import 'package:clout/screens/campaign_register/widgets/data_title.dart';
+import 'package:clout/screens/mypage/contract.dart';
 import 'package:flutter/material.dart';
 import 'package:clout/style.dart' as style;
 import 'package:get/get.dart';
@@ -52,7 +53,7 @@ class _SmallContractState extends State<SmallContract> {
           ],
         ),
         ElevatedButton(
-          onPressed: () => {},
+          onPressed: () => {Get.to(Contract())},
           style: ElevatedButton.styleFrom(
               backgroundColor: style.colors['main1'],
               shape: RoundedRectangleBorder(
