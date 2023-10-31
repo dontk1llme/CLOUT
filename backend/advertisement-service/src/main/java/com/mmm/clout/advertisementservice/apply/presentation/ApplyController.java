747c020aa0f6b32cdf70065190b4fa21365e4f69
윤다정 <ydajeong7@gmail.com>
2023-10-31 15:25:01 +0900

    S09P31A409-198 Chore: api 매핑 uri 변경
    - msa 구조로 uri 규칙 변경

--- backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/apply/presentation/ApplyController.java
+++ backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/apply/presentation/ApplyController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/applies")
+@RequestMapping("/v1/applies")
 @RequiredArgsConstructor
 public class ApplyController {
 
