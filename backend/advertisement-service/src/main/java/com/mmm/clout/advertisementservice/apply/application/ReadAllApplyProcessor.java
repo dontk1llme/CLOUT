ab117cdeda920a2e143bf34809ceeb1fa5b63af1
윤다정 <ydajeong7@gmail.com>
2023-11-01 15:43:00 +0900

    S09P31A409-198 Refactor: 카테고리별 신청 목록 구현 수정 (msa 통신 및 response 타입)
    - 이미지, star 기능 만들어지면 연결 필요

--- backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/apply/application/ReadAllApplyProcessor.java
+++ backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/apply/application/ReadAllApplyProcessor.java
@@ -1,14 +1,18 @@
 package com.mmm.clout.advertisementservice.apply.application;
 
+import com.mmm.clout.advertisementservice.apply.application.reader.ClouterApplyListReader;
 import com.mmm.clout.advertisementservice.apply.domain.Apply;
 import com.mmm.clout.advertisementservice.apply.domain.Apply.ApplyStatus;
 import com.mmm.clout.advertisementservice.apply.domain.info.AdvertiserInfo;
 import com.mmm.clout.advertisementservice.apply.domain.provider.AdvertiserProvider;
 import com.mmm.clout.advertisementservice.apply.domain.repository.ApplyRepository;
+import java.sql.Array;
+import java.util.ArrayList;
 import java.util.List;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -17,19 +21,19 @@ public class ReadAllApplyProcessor {
     private final ApplyRepository applyRepository;
     private final AdvertiserProvider advertiserInfoProvider;
 
-    public List<Apply> execute(Long applicantId, ApplyStatus applyStatus) {
+    @Transactional
+    public List<ClouterApplyListReader> execute(Long applicantId, ApplyStatus applyStatus) {
         List<Apply> applyList = applyRepository.getAllByStatus(applicantId, applyStatus);
-        applyList.stream().map(
-            apply -> {
-                Long advertiserId = apply.getCampaign().getAdvertiserId();
-                AdvertiserInfo info = advertiserInfoProvider.getAdvertiserInfoByMemberId(advertiserId);
-                return info;
-            }
-        ).forEach(
-            System.out::println
-        );
-
-
-        return null;
+        List<ClouterApplyListReader> readerList = new ArrayList<>();
+        for (Apply apply: applyList) {
+            Long advertiserId = apply.getCampaign().getAdvertiserId();
+            AdvertiserInfo info = advertiserInfoProvider.getAdvertiserInfoByMemberId(advertiserId);
+            readerList.add(
+                new ClouterApplyListReader(
+                    apply, info.getCompanyInfo().getCompanyName(), 0
+                )
+            );
+        }
+        return readerList;
     }
 }
