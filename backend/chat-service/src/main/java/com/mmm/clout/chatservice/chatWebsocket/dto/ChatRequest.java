ffa1be31724646c44f592492c357578c72d44d79
정승구 <jsggo2001@naver.com>
2023-11-02 13:32:32 +0900

    Refactor: [BE] 안쓰는 변수 제거

--- backend/chat-service/src/main/java/com/mmm/clout/chatservice/chatWebsocket/dto/ChatRequest.java
+++ backend/chat-service/src/main/java/com/mmm/clout/chatservice/chatWebsocket/dto/ChatRequest.java
@@ -2,8 +2,7 @@ package com.mmm.clout.chatservice.chatWebsocket.dto;
 
 public record ChatRequest(
         String sender,
-        String senderUuid,
-        String imgUrl,
+        String senderId,
         String msg
 ) {
 }
