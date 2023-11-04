7ce21c86a81f73d93c55629c7ce9df2f36cf0551
boyoun1113 <boyoun1113@naver.com>
2023-11-04 16:03:35 +0900

    Feat: [FE] ChoiceChip 위젯 제작

--- clout/lib/screens/mypage/contract_list.dart
+++ clout/lib/screens/mypage/contract_list.dart
@@ -1,12 +1,17 @@
 // 계약서 목록
-import 'package:clout/screens/mypage/widgets/contract_toggle.dart';
-import 'package:clout/screens/mypage/widgets/small_contract.dart';
+import 'package:clout/widgets/common/choicechip.dart';
 import 'package:flutter/material.dart';
 import 'package:clout/style.dart' as style;
 
 // widgets
 import 'package:clout/utilities/bouncing_listview.dart';
 import 'package:clout/widgets/header/header.dart';
+import 'package:clout/screens/mypage/widgets/small_contract.dart';
+
+class Contract {
+  String name = '못골영농조합법인';
+  String pay = '1,000 포인트'; // 포인트 또는 제공물품
+}
 
 class ContractList extends StatelessWidget {
   const ContractList({super.key});
@@ -21,33 +26,44 @@ class ContractList extends StatelessWidget {
             headerTitle: '내 계약서 목록',
           ),
         ),
+        backgroundColor: Colors.white,
         body: Container(
-            color: Colors.white,
-            width: double.infinity,
-            height: double.infinity,
-            child: BouncingListview(
-              child: FractionallySizedBox(
-                  widthFactor: 0.9,
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: EdgeInsets.only(bottom: 10),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [ContractToggle()],
-                        ),
-                      ),
-                      BouncingListview(
-                        child: Column(children: [
-                          for (num i = 0; i < 10; i++)
-                            Padding(
-                              padding: const EdgeInsets.only(top: 15),
-                              child: SmallContract(),
-                            ),
-                        ]),
-                      )
-                    ],
-                  )),
-            )));
+          color: Colors.white,
+          width: double.infinity,
+          height: double.infinity,
+          child: BouncingListview(
+            child: FractionallySizedBox(
+                widthFactor: 0.9,
+                child: Column(
+                  children: [
+                    ActionChoiceExample(
+                      labels: ['전체 내역', '서명 대기', '진행중', '계약 만료'],
+                      chipCount: 4,
+                    ),
+                    // ListView.builder(
+                    //   shrinkWrap: true,
+                    //   itemCount: 10,
+                    //   itemBuilder: (BuildContext context, int index) {
+                    //     return Padding(
+                    //       padding: EdgeInsets.symmetric(vertical: 7),
+                    //       child: SmallContract(),
+                    //     );
+                    //   },
+                    // ),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                    SmallContract(),
+                  ],
+                )),
+          ),
+        ));
   }
 }
