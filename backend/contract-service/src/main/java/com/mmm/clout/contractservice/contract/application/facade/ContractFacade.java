d31ff0fc33c5a8ac015bc33aaa68f598064a9cb6
정승구 <jsggo2001@naver.com>
2023-11-05 19:43:49 +0900

    Feat: [BE] 계약 주민번호 추가 구현

--- backend/contract-service/src/main/java/com/mmm/clout/contractservice/contract/application/facade/ContractFacade.java
+++ backend/contract-service/src/main/java/com/mmm/clout/contractservice/contract/application/facade/ContractFacade.java
@@ -1,8 +1,10 @@
 package com.mmm.clout.contractservice.contract.application.facade;
 
 import com.mmm.clout.contractservice.contract.application.CreateContractProcessor;
+import com.mmm.clout.contractservice.contract.application.UpdateRRNContractProcessor;
 import com.mmm.clout.contractservice.contract.application.command.CreateContractCommand;
 import com.mmm.clout.contractservice.contract.domain.Contract;
+import com.mmm.clout.contractservice.contract.presentation.response.UpdateRRNContractResponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.stereotype.Service;
@@ -13,9 +15,13 @@ import org.springframework.stereotype.Service;
 public class ContractFacade {
 
     private final CreateContractProcessor createContractProcessor;
+    private final UpdateRRNContractProcessor updateRRNContractProcessor;
 
     public Contract create(CreateContractCommand command) {
         return createContractProcessor.execute(command);
     }
 
+    public Contract updateRRN(Long id, String residentRegistrationNumber) {
+        return updateRRNContractProcessor.execute(id, residentRegistrationNumber);
+    }
 }
--- backend/contract-service/src/main/java/com/mmm/clout/contractservice/contract/presentation/ContractController.java
+++ backend/contract-service/src/main/java/com/mmm/clout/contractservice/contract/presentation/ContractController.java
@@ -3,14 +3,13 @@ package com.mmm.clout.contractservice.contract.presentation;
 import com.mmm.clout.contractservice.contract.application.facade.ContractFacade;
 import com.mmm.clout.contractservice.contract.presentation.docs.ContractControllerDocs;
 import com.mmm.clout.contractservice.contract.presentation.request.CreateContractRequest;
+import com.mmm.clout.contractservice.contract.presentation.request.UpdateRRNContractRequest;
 import com.mmm.clout.contractservice.contract.presentation.response.CreateContractResponse;
+import com.mmm.clout.contractservice.contract.presentation.response.UpdateRRNContractResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -31,4 +30,14 @@ public class ContractController implements ContractControllerDocs {
         return new ResponseEntity<>(result, HttpStatus.CREATED);
     }
 
+    @PatchMapping("/{contractId}")
+    public ResponseEntity<UpdateRRNContractResponse> updateRNN(
+            @PathVariable("contractId") Long id,
+            @RequestBody UpdateRRNContractRequest request
+            ) {
+        UpdateRRNContractResponse response = UpdateRRNContractResponse.from(
+                contractFacade.updateRRN(id, request.getResidentRegistrationNumber())
+        );
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
 }
