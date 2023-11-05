d3e7ebcad4b0ed5be7c5a3a13a5c357cf33f8e77
boyoun1113 <boyoun1113@naver.com>
2023-11-05 22:30:27 +0900

    Fix: [FE] 마이페이지 목록들 무한스크롤 수정
    - advertiser_likedclouters
    - clouter_likedcampaign
    - clouter_mycampaign

--- clout/lib/screens/mypage/clouter_mycampaign.dart
+++ clout/lib/screens/mypage/clouter_mycampaign.dart
@@ -47,21 +47,25 @@ class ClouterMyCampaign extends GetView<InfiniteScrollController> {
                 ),
               ),
               body: Padding(
-                padding: const EdgeInsets.all(10.0),
+                padding: const EdgeInsets.all(0),
                 // FilterButton(),
-                child: Obx(
-                  () => GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: screenWidth > 600 ? 4 : 2,
-                      crossAxisSpacing: 10,
-                      childAspectRatio: 0.7,
-                      mainAxisSpacing: screenWidth > 400 ? 15 : 10,
-                    ),
-                    controller: controller.scrollController.value,
-                    itemBuilder: (_, index) {
-                      print(controller.hasMore);
-                      if (index < controller.data.length) {
-                        return CampaignItemBox(
+                child: GridView.builder(
+                  physics: BouncingScrollPhysics(),
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: screenWidth > 600 ? 4 : 2,
+                    crossAxisSpacing: 0,
+                    childAspectRatio: 0.7,
+                    mainAxisSpacing: screenWidth > 400 ? 3 : 0,
+                  ),
+                  controller: controller.scrollController.value,
+                  itemBuilder: (_, index) {
+                    print(controller.hasMore);
+                    print(controller.data);
+                    print(controller.data.length);
+                    if (index < controller.data.length) {
+                      return Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: CampaignItemBox(
                           category: controller.data[index].category,
                           productName: controller.data[index].productName,
                           pay: controller.data[index].pay,
@@ -73,29 +77,29 @@ class ClouterMyCampaign extends GetView<InfiniteScrollController> {
                               controller.data[index].selectedPlatform,
                           starRating: controller.data[index].starRating,
                           firstImg: controller.data[index].firstImg,
-                        );
-                      }
-
-                      if (controller.hasMore || controller.isLoading) {
-                        return Center(child: RefreshProgressIndicator());
-                      }
-
-                      return Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text('데이터의 마지막 입니다'),
-                          IconButton(
-                            onPressed: () {
-                              controller.reload();
-                            },
-                            icon: Icon(Icons.refresh_outlined),
-                          ),
-                        ],
+                        ),
                       );
-                    },
-                    itemCount: controller.data.length + 1,
-                  ),
+                    }
+
+                    if (controller.hasMore || controller.isLoading) {
+                      return Center(child: RefreshProgressIndicator());
+                    }
+
+                    return Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text('데이터의 마지막 입니다'),
+                        IconButton(
+                          onPressed: () {
+                            controller.reload();
+                          },
+                          icon: Icon(Icons.refresh_outlined),
+                        ),
+                      ],
+                    );
+                  },
+                  itemCount: controller.data.length + 1,
                 ),
               ),
             ));
