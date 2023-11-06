12e4468d6c9f0ffbbb7ee089bab7bf5f7b964c84
boyoun1113 <boyoun1113@naver.com>
2023-11-06 13:05:11 +0900

    Fix: choicechip widget 적용
    - clouter_point_list
    - advertiser_point_list
    - contract_list
    - mypage 포인트관리 리스트 추가

--- clout/lib/screens/mypage/contract_list.dart
+++ clout/lib/screens/mypage/contract_list.dart
@@ -37,49 +37,56 @@ class ContractList extends GetView<ContractInfiniteScrollController> {
             headerTitle: '내 계약서 목록',
           ),
         ),
-        body: BouncingListview(
-          child: Column(
-            children: [
-              ActionChoiceExample(
-                labels: ['전체 내역', '서명 대기', '진행중', '계약 만료'],
-                chipCount: 4,
-              ),
-              ListView.builder(
-                shrinkWrap: true,
-                physics: BouncingScrollPhysics(),
-                controller: controller.scrollController.value,
-                itemBuilder: (_, index) {
-                  if (index < controller.data.length) {
-                    return Padding(
-                      padding:
-                          EdgeInsets.symmetric(horizontal: 20, vertical: 5),
-                      child: SmallContract(
-                          name: controller.data[index].name,
-                          pay: controller.data[index].pay),
-                    );
-                  }
-                  if (controller.hasMore || controller.isLoading) {
-                    return Center(child: RefreshProgressIndicator());
-                  }
+        body: Container(
+          color: Colors.white,
+          width: double.infinity,
+          child: BouncingListview(
+            child: FractionallySizedBox(
+              alignment: Alignment.center,
+              widthFactor: 0.9,
+              child: Column(
+                children: [
+                  ActionChoiceExample(
+                    labels: ['전체 내역', '서명 대기', '진행중', '계약 만료'],
+                    chipCount: 4,
+                  ),
+                  ListView.builder(
+                    shrinkWrap: true,
+                    physics: BouncingScrollPhysics(),
+                    controller: controller.scrollController.value,
+                    itemBuilder: (_, index) {
+                      if (index < controller.data.length) {
+                        return Padding(
+                          padding: EdgeInsets.symmetric(vertical: 5),
+                          child: SmallContract(
+                              name: controller.data[index].name,
+                              pay: controller.data[index].pay),
+                        );
+                      }
+                      if (controller.hasMore || controller.isLoading) {
+                        return Center(child: RefreshProgressIndicator());
+                      }
 
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text('데이터의 마지막 입니다'),
-                      IconButton(
-                        onPressed: () {
-                          controller.reload();
-                        },
-                        icon: Icon(Icons.refresh_outlined),
-                      ),
-                    ],
-                  );
-                },
-                itemCount: controller.data.length + 1,
+                      return Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('데이터의 마지막 입니다'),
+                          IconButton(
+                            onPressed: () {
+                              controller.reload();
+                            },
+                            icon: Icon(Icons.refresh_outlined),
+                          ),
+                        ],
+                      );
+                    },
+                    itemCount: controller.data.length + 1,
+                  ),
+                  SizedBox(height: 30),
+                ],
               ),
-              SizedBox(height: 30),
-            ],
+            ),
           ),
         ),
       ),
