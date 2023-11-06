0e7b360572c8fbed105c5c6a7cf30b29fa96ea3d
boyoun1113 <boyoun1113@naver.com>
2023-11-06 10:56:32 +0900

    Fix: notification_item_box widget 수정
    - 반응형 수정
    - 계약 상태에 따라 조건부 렌더링

--- clout/lib/screens/notification/widgets/notification_item_box.dart
+++ clout/lib/screens/notification/widgets/notification_item_box.dart
@@ -1,3 +1,4 @@
+import 'package:clout/screens/campaign_list/campaign_list.dart';
 import 'package:clout/screens/mypage/contract.dart';
 import 'package:flutter/material.dart';
 import 'package:clout/style.dart' as style;
@@ -8,7 +9,17 @@ import 'package:clout/screens/review_register/review_register.dart';
 import 'package:clout/screens/clouter/clouter_select.dart';
 
 class NotificationItem extends StatefulWidget {
-  const NotificationItem({super.key});
+  final int result;
+  final String image;
+  final String name;
+
+  // 📌 result가 -1 이면 계약 불발, 0이면 계약서 작성, 1이면 계약 성사 완료
+
+  const NotificationItem(
+      {super.key,
+      required this.result,
+      required this.name,
+      required this.image});
 
   @override
   State<NotificationItem> createState() => _NotificationItemState();
@@ -20,7 +31,7 @@ class _NotificationItemState extends State<NotificationItem> {
     final screenWidth = MediaQuery.of(context).size.width;
 
     return Container(
-      padding: EdgeInsets.all(10),
+      padding: EdgeInsets.all(20),
       margin: EdgeInsets.symmetric(
           horizontal: screenWidth > 400 ? 15 : 10,
           vertical: screenWidth > 400 ? 7 : 4),
@@ -30,65 +41,121 @@ class _NotificationItemState extends State<NotificationItem> {
       ),
       child: SizedBox(
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisAlignment: screenWidth > 400
+              ? MainAxisAlignment.start
+              : MainAxisAlignment.center,
           children: [
             ClipRRect(
               borderRadius: BorderRadius.circular(5), // 5% of screen width
               child: Image.asset(
-                'assets/images/itemImage.jpg',
-                width: 130,
-                height: 130,
+                widget.image,
+                width: 100,
+                height: 100,
                 fit: BoxFit.cover,
               ),
             ),
             SizedBox(
               width: 20,
             ),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('업체명',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w700,
-                      fontSize: 20,
-                    )),
-                Text('계약서가 도착했어요!',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      fontWeight: FontWeight.w600,
-                      fontSize: 18,
-                    )),
-                Text('계약내용을 확인해보세요',
-                    maxLines: 1,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(
-                      color: style.colors['gray'],
-                      fontSize: 15,
-                    )),
-                SizedBox(
-                  width: 200,
-                  child: ElevatedButton(
-                    onPressed: () {
-                      Get.to(() => Contract());
-                    },
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all<Color>(
-                        Color(0xFF6B4EFF),
-                      ),
-                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                          borderRadius:
-                              BorderRadius.circular(screenWidth * 0.1),
+            SizedBox(
+              width: screenWidth - 200,
+              child: Wrap(
+                // runAlignment: WrapAlignment.spaceBetween,
+                // runSpacing: 30,
+                // spacing: screenWidth - 600,
+                alignment: WrapAlignment.spaceBetween,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(widget.name,
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w700,
+                            fontSize: 20,
+                          )),
+                      Text(
+                          widget.result != -1
+                              ? widget.result == 1
+                                  ? '계약이 성사되었어요! 🎉'
+                                  : '계약서가 도착했어요! 💌'
+                              : '계약이 불발되었어요 😥',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          )),
+                      Text(
+                          widget.result != -1
+                              ? widget.result == 1
+                                  ? '광고 효과를 기대해보세요'
+                                  : '계약내용을 확인해보세요'
+                              : '더 좋은 계약을 하실 수 있을거에요',
+                          maxLines: 1,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            color: style.colors['gray'],
+                            fontSize: 14,
+                          )),
+                      // SizedBox(
+                      //   width: 200,
+                      //   child: ElevatedButton(
+                      //     onPressed: () {
+                      //       Get.to(() =>
+                      //           widget.result != -1 ? Contract() : CampaignList());
+                      //     },
+                      //     style: ButtonStyle(
+                      //       backgroundColor: MaterialStateProperty.all<Color>(
+                      //         Color(0xFF6B4EFF),
+                      //       ),
+                      //       shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      //         RoundedRectangleBorder(
+                      //           borderRadius:
+                      //               BorderRadius.circular(screenWidth * 0.1),
+                      //         ),
+                      //       ),
+                      //     ),
+                      //     child: Text(widget.result != -1
+                      //         ? widget.result == 1
+                      //             ? '계약서 열람하기'
+                      //             : '계약서 작성하기'
+                      //         : '다른 캠페인 찾기'),
+                      //   ),
+                      // )
+                    ],
+                  ),
+                  // SizedBox(width: screenWidth - 600),
+                  SizedBox(
+                    width: 200,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Get.to(() =>
+                            widget.result != -1 ? Contract() : CampaignList());
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                          Color(0xFF6B4EFF),
+                        ),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius:
+                                BorderRadius.circular(screenWidth * 0.1),
+                          ),
                         ),
                       ),
+                      child: Text(widget.result != -1
+                          ? widget.result == 1
+                              ? '계약서 열람하기'
+                              : '계약서 작성하기'
+                          : '다른 캠페인 찾기'),
                     ),
-                    child: Text('계약서 확인하기'),
                   ),
-                )
-              ],
+                ],
+              ),
             )
           ],
         ),
