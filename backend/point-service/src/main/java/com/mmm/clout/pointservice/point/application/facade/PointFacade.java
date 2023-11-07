822e8a2732122e956c13fd492d4421398b870c77
윤다정 <ydajeong7@gmail.com>
2023-11-07 16:30:07 +0900

    S09P31A409-162 Feat: 포인트 출금 구조

--- backend/point-service/src/main/java/com/mmm/clout/pointservice/point/application/facade/PointFacade.java
+++ backend/point-service/src/main/java/com/mmm/clout/pointservice/point/application/facade/PointFacade.java
@@ -2,8 +2,10 @@ package com.mmm.clout.pointservice.point.application.facade;
 
 import com.mmm.clout.pointservice.point.application.ChargePointProcessor;
 import com.mmm.clout.pointservice.point.application.ReducePointProcessor;
+import com.mmm.clout.pointservice.point.application.WithdrawPointProcessor;
 import com.mmm.clout.pointservice.point.application.command.ChargeCommand;
 import com.mmm.clout.pointservice.point.application.command.ReduceCommand;
+import com.mmm.clout.pointservice.point.application.command.WithdrawCommand;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ public class PointFacade {
 
     private final ChargePointProcessor chargePointProcessor;
     private final ReducePointProcessor reducePointProcessor;
+    private final WithdrawPointProcessor withdrawPointProcessor;
 
     public void charge(ChargeCommand command) {
         chargePointProcessor.execute(command.getMemberId(), command.getChargePoint(),  command.getPaymentType());
@@ -20,5 +23,9 @@ public class PointFacade {
 
     public void reduce(ReduceCommand command) {
         reducePointProcessor.execute(command.getMemberId(), command.getReducingPoint(), command.getPointCategory());
+    }
+
+    public void withdraw(WithdrawCommand command) {
+        withdrawPointProcessor.execute(command);
     }
 }
