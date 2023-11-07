07ac13f51798317652fbe72ddb4fc84eba3491ae
김민식 <sakmsik98@naver.com>
2023-11-08 02:32:49 +0900

    Feat: Eureka dependence

--- backend/member-service/src/main/java/com/mmm/clout/memberservice/UserserviceApplication.java
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/UserserviceApplication.java
@@ -3,10 +3,12 @@ package com.mmm.clout.memberservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
+@EnableDiscoveryClient
 public class UserserviceApplication {
 
 	public static void main(String[] args) {
