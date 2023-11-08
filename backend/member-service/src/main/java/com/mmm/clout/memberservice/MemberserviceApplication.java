38942e05ffc9c03af481504576f6a68bf4a8bc78
김민식 <sakmsik98@naver.com>
2023-11-08 09:51:29 +0900

    S09P31A409-288 Fix: conflict

--- backend/member-service/src/main/java/com/mmm/clout/memberservice/MemberserviceApplication.java
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/MemberserviceApplication.java
@@ -3,16 +3,14 @@ package com.mmm.clout.memberservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaAuditing
-@EnableDiscoveryClient
-public class UserserviceApplication {
+public class MemberserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
+		SpringApplication.run(MemberserviceApplication.class, args);
 	}
 
 }
