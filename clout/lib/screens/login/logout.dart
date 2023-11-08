161e25e31fcd2a81748632b1182893467ac409a5
dontk1llme <letm2go@gmail.com>
2023-11-08 11:11:02 +0900

    Feat: 로그아웃 됨

--- clout/lib/screens/login/logout.dart
+++ clout/lib/screens/login/logout.dart
@@ -1,22 +1,30 @@
+import 'package:clout/hooks/logout_api.dart';
 import 'package:clout/providers/user_controllers/user_controller.dart';
 import 'package:get/get.dart';
 
-void logout() {
+void logout() async {
   final userController = Get.find<UserController>();
+  // 0. api에 전송
+  final LogoutApi loginApi = LogoutApi();
+  loginApi.postRequest('/v1/members/logout', userController.userLogin['authorization']);
+  // 백에선 auth만 넘기면 자동으로 삭제해 준다네요
+
   // 1. 로그인 정보 삭제
   var userLogout = {
         'login_success': false,
-        'authorization': null,
-        'refresh_token': null,
-        'clout_or_adv': null,
+        'authorization': '',
+        'refresh_token': '',
+        'clout_or_adv': 0,
   };
   userController.setUserLogin(userLogout);
   
   // 2. 유저 정보 삭제
-  userController.setUserId(null);
-  userController.setPassword(null);
+  userController.setUserId('');
+  userController.setPassword('');
   userController.setUserInfo();
   
-  // 3. 리디렉션 또는 홈으로 이동
+  // 3. 홈으로 이동
+  Get.offAllNamed('/');
+  
   
 }
