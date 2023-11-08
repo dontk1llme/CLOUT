f143f641567940a55d26bbc378be058477a49acb
boyoun1113 <boyoun1113@naver.com>
2023-11-08 16:11:52 +0900

    Fix: infiniteScroll 수정
    - campaign_infinite_scroll_body 수정
    - infinite_scroll_controller 수정
    - advertiser_mycampaign 페이지에 적용
    "

--- clout/lib/screens/mypage/advertiser/advertiser_mycampaign.dart
+++ clout/lib/screens/mypage/advertiser/advertiser_mycampaign.dart
@@ -2,12 +2,14 @@ import 'package:clout/providers/user_controllers/user_controller.dart';
 import 'package:clout/screens/list/widgets/campaign_infinite_scroll_body.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:clout/style.dart' as style;
 
 // widgets
 import 'package:clout/widgets/header/header.dart';
 
 // controllers
 import 'package:clout/providers/scroll_controllers/infinite_scroll_controller.dart';
+import 'package:loading_indicator/loading_indicator.dart';
 
 class AdvertiserMycampaign extends GetView<InfiniteScrollController> {
   AdvertiserMycampaign({super.key});
@@ -19,6 +21,7 @@ class AdvertiserMycampaign extends GetView<InfiniteScrollController> {
 
   @override
   Widget build(BuildContext context) {
+    infiniteController.setCurrentPage(0);
     infiniteController.setEndPoint('/v1/advertisements/advertisements');
     infiniteController.setParameter(
         '?advertiserId=${userController.userId}&page=${infiniteController.currentPage}&size=${10}');
@@ -34,12 +37,61 @@ class AdvertiserMycampaign extends GetView<InfiniteScrollController> {
             headerTitle: '내 캠페인 목록',
           ),
         ),
-        body: Padding(
-          padding: const EdgeInsets.all(0),
-          // FilterButton(),
-          child:
+        body: SingleChildScrollView(
+          controller: controller.scrollController.value,
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            children: [
               CampaignInfiniteScrollBody(controllerTag: 'advertiserMyCampaign'),
+              infiniteController.hasMore
+                  ? Padding(
+                      padding: const EdgeInsets.only(top: 20, bottom: 40),
+                      child: SizedBox(
+                        height: 50,
+                        child: Center(
+                            child: LoadingIndicator(
+                          indicatorType: Indicator.ballRotateChase,
+                          colors: [
+                            style.colors['main1-4']!,
+                            style.colors['main1-3']!,
+                            style.colors['main1-2']!,
+                            style.colors['main1-1']!,
+                            style.colors['main1']!,
+                          ],
+                        )),
+                      ),
+                    )
+                  : Container(
+                      height: 30,
+                    )
+            ],
+          ),
         ),
+        // body: ListView(
+        //   // FilterButton(),
+        //   children: [
+        //     CampaignInfiniteScrollBody(controllerTag: 'advertiserMyCampaign'),
+        //     infiniteController.isLoading
+        //         ? Padding(
+        //             padding: const EdgeInsets.only(top: 20, bottom: 40),
+        //             child: SizedBox(
+        //               height: 50,
+        //               child: Center(
+        //                   child: LoadingIndicator(
+        //                 indicatorType: Indicator.ballRotateChase,
+        //                 colors: [
+        //                   style.colors['main1-4']!,
+        //                   style.colors['main1-3']!,
+        //                   style.colors['main1-2']!,
+        //                   style.colors['main1-1']!,
+        //                   style.colors['main1']!,
+        //                 ],
+        //               )),
+        //             ),
+        //           )
+        //         : Container()
+        //   ],
+        // ),
       ),
     );
   }
