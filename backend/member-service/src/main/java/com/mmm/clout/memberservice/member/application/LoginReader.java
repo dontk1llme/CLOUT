c2caf38c81ccfc99abf842f4f28c5a48c56051f3
정승구 <jsggo2001@naver.com>
2023-11-09 18:07:48 +0900

    Feat: [BE] 로그인 리스폰스 id값 추가 HotFix

--- backend/member-service/src/main/java/com/mmm/clout/memberservice/member/application/LoginReader.java
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/member/application/LoginReader.java
@@ -2,6 +2,7 @@ package com.mmm.clout.memberservice.member.application;
 
 import com.mmm.clout.memberservice.common.Role;
 import com.mmm.clout.memberservice.member.infrastructure.auth.dto.AuthDto;
+import com.mmm.clout.memberservice.member.presentation.response.LoginReseponse;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -12,4 +13,13 @@ public class LoginReader {
     private AuthDto.TokenDto TokenDto;
 
     private Role memberRole;
+
+    private Long memberId;
+
+    public LoginReseponse toResponse() {
+        return new LoginReseponse(
+            this.memberRole,
+            this.memberId
+        );
+    }
 }
