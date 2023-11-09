4042584742538aad52bf4a30eaf5db7eb98f2497
윤다정 <ydajeong7@gmail.com>
2023-11-09 13:08:29 +0900

    Fix: lazy loading

--- backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/advertisements/application/GetCampaignListByAdvertiser.java
+++ backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/advertisements/application/GetCampaignListByAdvertiser.java
@@ -7,7 +7,9 @@ import com.mmm.clout.advertisementservice.common.msa.info.AdvertiserInfo;
 import com.mmm.clout.advertisementservice.common.msa.provider.MemberProvider;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.hibernate.Hibernate;
 import org.springframework.data.domain.Pageable;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 public class GetCampaignListByAdvertiser {
@@ -15,10 +17,14 @@ public class GetCampaignListByAdvertiser {
     private final CampaignRepository campaignRepository;
     private final MemberProvider memberProvider;
 
+    @Transactional
     public CampaignListReader execute(Long advertiserId, Pageable pageable) {
         AdvertiserInfo advertiserInfo = memberProvider.getAdvertiserInfoByMemberId(advertiserId);
         List<Campaign> campainList =
             campaignRepository.getCampainListByAdvertiserId(advertiserId, pageable);
+        for (Campaign campaign : campainList) {
+            campaign.readTransaction();
+        }
         return new CampaignListReader(campainList, advertiserInfo);
     }
 }
