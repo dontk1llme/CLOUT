46056fbbc865f437202571f27be81a4622f6f0c8
윤다정 <ydajeong7@gmail.com>
2023-11-13 17:12:18 +0900

    Docs: apply swagger

--- backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/common/docs/ApplyControllerDocs.java
+++ backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/common/docs/ApplyControllerDocs.java
@@ -3,11 +3,16 @@ package com.mmm.clout.advertisementservice.common.docs;
 import com.mmm.clout.advertisementservice.advertisements.persentation.request.CreateCampaignRequest;
 import com.mmm.clout.advertisementservice.advertisements.persentation.request.UpdateCampaignRequest;
 import com.mmm.clout.advertisementservice.advertisements.persentation.response.CreateCampaignResponse;
+import com.mmm.clout.advertisementservice.advertisements.persentation.response.CustomPageResponse;
 import com.mmm.clout.advertisementservice.advertisements.persentation.response.DeleteCampaignResponse;
 import com.mmm.clout.advertisementservice.advertisements.persentation.response.UpdateCampaignResponse;
+import com.mmm.clout.advertisementservice.apply.domain.Apply.ApplyStatus;
 import com.mmm.clout.advertisementservice.apply.presentation.request.CreateApplyRequest;
+import com.mmm.clout.advertisementservice.apply.presentation.response.ApplicantResponse;
+import com.mmm.clout.advertisementservice.apply.presentation.response.ApplyMessageResponse;
 import com.mmm.clout.advertisementservice.apply.presentation.response.CreateApplyResponse;
 import com.mmm.clout.advertisementservice.apply.presentation.response.GetAllByStatusResponse;
+import com.mmm.clout.advertisementservice.apply.presentation.response.GetApplyByStatusResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.enums.ParameterIn;
@@ -112,11 +117,73 @@ public interface ApplyControllerDocs {
             )
         }
     )
-    ResponseEntity<GetAllByStatusResponse> getApplyListByStatus(
+    ResponseEntity<CustomPageResponse<GetApplyByStatusResponse>> getApplyListByStatus(
         @RequestParam Long clouterId,
-        @RequestParam String type,
+        @RequestParam ApplyStatus type,
         @RequestParam(defaultValue = "0") int page,
         @RequestParam(defaultValue = "10") int size
     );
+
+
+    @Operation(summary = "광고주가) 해당 광고 신청자 목록 조회",
+        description = "광고주가) 해당 광고 신청자 목록을 조회합니다.",
+        parameters = {
+            @Parameter(
+                in = ParameterIn.HEADER,
+                name = "Authorization",
+                required = true,
+                schema = @Schema(type = "string"),
+                description = "인증 토큰"
+            ),
+            @Parameter(
+                in = ParameterIn.QUERY,
+                name = "advertisementId",
+                required = true,
+                description = "광고 캠페인 고유 식별자(id)"
+            ),
+            @Parameter(
+                in = ParameterIn.QUERY,
+                name = "page",
+                required = true,
+                description = "페이지"
+            ),
+            @Parameter(
+                in = ParameterIn.QUERY,
+                name = "size",
+                required = true,
+                description = "페이지당 보여줄 데이터 사이즈"
+            )
+        }
+    )
+    ResponseEntity<CustomPageResponse<ApplicantResponse>> getApplicantList(
+        @RequestParam Long advertisementId,
+        @RequestParam(defaultValue = "0") int page,
+        @RequestParam(defaultValue = "10") int size
+    );
+
+    @Operation(summary = "신청자 한마디 조회",
+        description = "신청 id에 해당하는 신청자의 한마디 메시지를 조회합니다.",
+        parameters = {
+            @Parameter(
+                in = ParameterIn.HEADER,
+                name = "Authorization",
+                required = true,
+                schema = @Schema(type = "string"),
+                description = "인증 토큰"
+            ),
+            @Parameter(
+                in = ParameterIn.PATH,
+                name = "applyId",
+                required = true,
+                description = "신청 고유 식별자(id)"
+            )
+        }
+    )
+    ResponseEntity<ApplyMessageResponse> getApplyMsg(
+        @PathVariable Long applyId
+    );
+
+
+
 
 }
