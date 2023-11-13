5220005ccf362ad3f95c41e000f6ea0a865b9604
김민식 <sakmsik98@naver.com>
2023-11-14 02:17:09 +0900

    Feat: Add Image uplader

--- backend/member-service/src/main/java/com/mmm/clout/memberservice/clouter/application/CreateClouterProcessor.java
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/clouter/application/CreateClouterProcessor.java
@@ -5,6 +5,9 @@ import com.mmm.clout.memberservice.clouter.application.command.CreateClrCommand;
 import com.mmm.clout.memberservice.clouter.domain.Clouter;
 import com.mmm.clout.memberservice.clouter.domain.repository.ClouterRepository;
 import com.mmm.clout.memberservice.clouter.domain.exception.ClrIdDuplicateException;
+import com.mmm.clout.memberservice.image.domain.FileUploader;
+import com.mmm.clout.memberservice.image.domain.Image;
+import com.mmm.clout.memberservice.image.domain.repository.ImageRepository;
 import com.mmm.clout.memberservice.member.domain.Member;
 import com.mmm.clout.memberservice.member.domain.info.CreatePointRequest;
 import com.mmm.clout.memberservice.member.domain.provider.PointProvider;
@@ -14,6 +17,9 @@ import com.mmm.clout.memberservice.star.domain.repository.StarRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 public class CreateClouterProcessor {
@@ -23,15 +29,30 @@ public class CreateClouterProcessor {
     private final BCryptPasswordEncoder encoder;
     private final StarRepository starRepository;
     private final PointProvider pointProvider;
+    private final FileUploader fileUploader;
+    private final ImageRepository imageRepository;
 
     @Transactional
-    public Clouter execute(CreateClrCommand command) {
+    public Clouter execute(CreateClrCommand command, List<MultipartFile> files) throws Exception {
         checkUserId(command.getUserId());
         Clouter clouter = command.toEntity();
         clouter.changePwd(encoder.encode(clouter.getPwd()));
         Clouter savedClouter = clouterRepository.save(clouter);
         initStar(savedClouter);
         initPoint(savedClouter);
+
+        //file uploader
+        for(MultipartFile multipartFile : files) {
+            String originalName = multipartFile.getOriginalFilename();
+            String uploadedPath = fileUploader.upload(multipartFile, savedClouter.getId());
+            Image image = Image.create(
+                    savedClouter,
+                    originalName,
+                    "https://yangkidsbucket.s3.ap-northeast-2.amazonaws.com/" + uploadedPath,
+                    uploadedPath
+            );
+            imageRepository.save(image);
+        }
         return savedClouter;
     }
 
---
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/image/infrastructure/persistance/jpa/JpaImageRepository.java
@@ -0,0 +1,12 @@
+package com.mmm.clout.memberservice.image.infrastructure.persistance.jpa;
+
+import com.mmm.clout.memberservice.image.domain.Image;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface JpaImageRepository extends JpaRepository<Image, Long> {
+
+        List<Image> findByMember_Id(Long Id);
+
+}
