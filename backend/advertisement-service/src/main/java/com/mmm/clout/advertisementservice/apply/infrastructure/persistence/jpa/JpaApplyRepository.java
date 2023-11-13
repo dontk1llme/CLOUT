7166fd658eed2b024cb85b4b4419d15ca9d7db9b
윤다정 <ydajeong7@gmail.com>
2023-11-13 15:48:48 +0900

    Refactor: 클라우터 신청 목록 페이지네이션 정보 추가

--- backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/apply/infrastructure/persistence/jpa/JpaApplyRepository.java
+++ backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/apply/infrastructure/persistence/jpa/JpaApplyRepository.java
@@ -4,13 +4,15 @@ import com.mmm.clout.advertisementservice.advertisements.domain.Campaign;
 import com.mmm.clout.advertisementservice.apply.domain.Apply;
 import com.mmm.clout.advertisementservice.apply.domain.Apply.ApplyStatus;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface JpaApplyRepository extends JpaRepository<Apply, Long> {
 
     boolean existsByCampaignAndApplicant_ApplicantId(Campaign campaign, Long clouterId);
 
-    List<Apply> findByApplicant_ApplicantIdAndApplyStatus(Long applicantId, ApplyStatus applyStatus);
+    Page<Apply> findByApplicant_ApplicantIdAndApplyStatus(Long applicantId, ApplyStatus applyStatus, Pageable pageable);
 
     List<Apply> findByCampaignId(Long advertisementId);
 
