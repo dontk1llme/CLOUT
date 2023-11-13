260fba3489eb29fd3ab3c4171e4d1b6b20ce4b62
윤다정 <ydajeong7@gmail.com>
2023-11-13 09:29:49 +0900

    Refactor: 포인트 생성 로직 변경 및 create api 추가

--- backend/point-service/src/main/java/com/mmm/clout/pointservice/point/application/AddPointProcessor.java
+++ backend/point-service/src/main/java/com/mmm/clout/pointservice/point/application/AddPointProcessor.java
@@ -3,6 +3,7 @@ package com.mmm.clout.pointservice.point.application;
 import com.mmm.clout.pointservice.point.application.command.AddPointCommand;
 import com.mmm.clout.pointservice.point.domain.Point;
 import com.mmm.clout.pointservice.point.domain.PointTransaction;
+import com.mmm.clout.pointservice.point.domain.exception.PointNotFoundException;
 import com.mmm.clout.pointservice.point.domain.repository.PointRepository;
 import com.mmm.clout.pointservice.point.domain.repository.PointTransactionRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,7 +18,7 @@ public class AddPointProcessor {
     @Transactional
     public PointTransaction execute(AddPointCommand command) {
         Point point = pointRepository.findByMemberId(command.getMemberId())
-            .orElseGet(() -> pointRepository.save(Point.create(command.getMemberId(), 0L)));
+            .orElseThrow(PointNotFoundException::new);
         point.add(command.getAddingPoint());
         PointTransaction pts = PointTransaction.add(point, command.getAddingPoint(), command.getPointCategory(), command.getCounterParty());
         return pointTransactionRepository.save(pts);
