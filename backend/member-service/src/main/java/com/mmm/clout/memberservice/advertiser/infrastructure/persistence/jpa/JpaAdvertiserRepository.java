51e4a4add77b3fb31cae409ca469d2c0167fb9cf
정승구 <jsggo2001@naver.com>
2023-11-14 15:41:21 +0900

    Feat: [BE] 폰번호 유니크 및 검증 api 구현

--- backend/member-service/src/main/java/com/mmm/clout/memberservice/advertiser/infrastructure/persistence/jpa/JpaAdvertiserRepository.java
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/advertiser/infrastructure/persistence/jpa/JpaAdvertiserRepository.java
@@ -5,8 +5,11 @@ import com.mmm.clout.memberservice.clouter.domain.Clouter;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JpaAdvertiserRepository extends JpaRepository<Advertiser, Long> {
 
     List<Advertiser> findByIdIn(List<Long> idList);
+
+    Optional<Advertiser> findByCompanyInfo_ManagerPhoneNumber(String phoneNumber);
 }
