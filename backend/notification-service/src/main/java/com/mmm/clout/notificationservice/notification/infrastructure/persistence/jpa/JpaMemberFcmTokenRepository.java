0e67c18b48c0093f5bfa628361bc1b5a123100d5
윤다정 <ydajeong7@gmail.com>
2023-11-14 14:32:38 +0900

    Feat: 유저 알림 토큰 생성 및 저장 api

---
+++ backend/notification-service/src/main/java/com/mmm/clout/notificationservice/notification/infrastructure/persistence/jpa/JpaMemberFcmTokenRepository.java
@@ -0,0 +1,12 @@
+package com.mmm.clout.notificationservice.notification.infrastructure.persistence.jpa;
+
+import com.mmm.clout.notificationservice.notification.domain.entity.MemberFcmToken;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaMemberFcmTokenRepository extends JpaRepository<MemberFcmToken, Long> {
+
+    Optional<MemberFcmToken> findByMemberIdAndFcmToken(Long memberId, String fcmToken);
+
+    Optional<MemberFcmToken> findByMemberIdAndDeviceId(Long memberId, String deviceId);
+}
