1086351968b08135930e8916ae043b48a553909b
정승구 <jsggo2001@naver.com>
2023-11-14 10:49:11 +0900

    Feat: [BE] 북마크 체크 여부 api 구현

--- backend/member-service/src/main/java/com/mmm/clout/memberservice/bookmark/application/CheckBookmarkProcessor.java
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/bookmark/application/CheckBookmarkProcessor.java
@@ -1,6 +1,5 @@
 package com.mmm.clout.memberservice.bookmark.application;
 
-import com.mmm.clout.memberservice.bookmark.application.command.BookmarkDeleteCommand;
 import com.mmm.clout.memberservice.bookmark.domain.Bookmark;
 import com.mmm.clout.memberservice.bookmark.domain.exception.NotFoundBookmarkException;
 import com.mmm.clout.memberservice.bookmark.domain.repository.BookmarkRepository;
@@ -8,20 +7,16 @@ import com.mmm.clout.memberservice.member.domain.Member;
 import com.mmm.clout.memberservice.member.domain.repository.MemberRepository;
 import com.mmm.clout.memberservice.star.domain.exception.NotFoundMemberException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
-public class DeleteBookmarkProcessor {
+public class CheckBookmarkProcessor {
 
     private final BookmarkRepository bookmarkRepository;
     private final MemberRepository memberRepository;
 
-    @Transactional
-    public Long execute(BookmarkDeleteCommand command) {
-        Member member = memberRepository.findById(command.getMemberId()).orElseThrow(NotFoundMemberException::new);
-        Bookmark findBookmark = bookmarkRepository.findByMemberAndTargetId(member, command.getTargetId()).orElseThrow(() -> new NotFoundBookmarkException());
-        Long bookmarkId = findBookmark.getId();
-        bookmarkRepository.delete(findBookmark);
-        return bookmarkId;
+    public boolean execute(Long memberId, Long targetId) {
+        Member findMember = memberRepository.findById(memberId).orElseThrow(NotFoundMemberException::new);
+        Bookmark findBookmark = bookmarkRepository.findByMemberAndTargetId(findMember, targetId).orElse(null);
+        return findBookmark != null ? true : false;
     }
 }
