cbe62e456b0b85552a5fd37d52a04e46040bb1bb
윤다정 <ydajeong7@gmail.com>
2023-11-14 14:57:57 +0900

    Fix: consumer group 설정

--- backend/notification-service/src/main/java/com/mmm/clout/notificationservice/notification/presentation/test/KafkaConsumer.java
+++ backend/notification-service/src/main/java/com/mmm/clout/notificationservice/notification/presentation/test/KafkaConsumer.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class KafkaConsumer {
 
-    @KafkaListener(topics = "${message.topic.name}")
+    @KafkaListener(topics = "${message.topic.name}", groupId = "${spring.kafka.consumer.group-id}")
     public void consume(String message) throws IOException {
         log.info(String.format("Consumed message : %s", message));
     }
