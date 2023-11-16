c8bcdebc71b9061dde7df6c8ee5cbd3d31c99dd5
정승구 <jsggo2001@naver.com>
2023-11-16 14:15:00 +0900

    Refactor: [BE] 북마크 광고 리스폰스 구조 수정

--- backend/member-service/src/main/java/com/mmm/clout/memberservice/bookmark/application/reader/CampaignReader.java
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/bookmark/application/reader/CampaignReader.java
@@ -2,9 +2,10 @@ package com.mmm.clout.memberservice.bookmark.application.reader;
 
 import com.mmm.clout.memberservice.advertiser.domain.Advertiser;
 import com.mmm.clout.memberservice.bookmark.domain.info.CampaignInfo;
-import com.mmm.clout.memberservice.image.domain.Image;
+import com.mmm.clout.memberservice.bookmark.presentation.response.AdvertiserResponse;
+import com.mmm.clout.memberservice.bookmark.presentation.response.CampaignResponse;
+import com.mmm.clout.memberservice.bookmark.presentation.response.CompanyResponse;
 import com.mmm.clout.memberservice.image.presentation.ImageResponse;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import java.time.LocalDate;
@@ -14,76 +15,30 @@ import java.util.List;
 @Getter
 public class CampaignReader {
 
-    private Long campaignId;
+    private CampaignResponse campaign;
 
-    private List<String> adPlatformList;
+    private AdvertiserResponse advertiserInfo;
 
-    private Long price;
-
-    private String details;
-
-    private LocalDateTime deletedAt;
-
-    private String title;
-
-    private String adCategory; // 광고 카테고리
-
-    private Boolean isPriceChangeable; // 광고비 협의 여부
-
-    private Boolean isDeliveryRequired; // 배송 여부
-
-    private Integer numberOfRecruiter; // 모집인원
-
-    private Integer numberOfApplicants; // 신청인원
-
-    private Integer numberOfSelectedMembers; // 채택 인원
-
-    private String offeringDetails; // 제공 내역 설명
-
-    private String sellingLink; // 판매처 링크 (선택사항)
-
-    private LocalDate applyStartDate; // 모집 시작 날짜
-
-    private LocalDate applyEndDate; // 모집 종료 날짜
-
-    private Integer minClouterAge; // 최소 클라우터 나이
-
-    private Integer maxClouterAge; // 최대 클라우터 나이
-
-    private Integer minFollower; // 최소 팔로워 수
-
-    private Boolean isEnded; // 모집 종료 여부
-
-    private String companyName;
-
-    private Long companyAvgScore;
-
-    private List<ImageResponse> imageInfo;//이미지 경로
+    private List<ImageResponse> imageList;//이미지 경로
 
     public CampaignReader(CampaignInfo info, Advertiser advertiser) {
-        this.campaignId = info.getCampaignId();
-        this.adPlatformList = info.getAdPlatformList();
-        this.price = info.getPrice();
-        this.details = info.getDetails();
-        this.deletedAt = info.getDeletedAt();
-        this.title = info.getTitle();
-        this.adCategory = info.getAdCategory();
-        this.isPriceChangeable = info.getIsPriceChangeable();
-        this.isDeliveryRequired = info.getIsDeliveryRequired();
-        this.numberOfRecruiter = info.getNumberOfRecruiter();
-        this.numberOfApplicants = info.getNumberOfApplicants();
-        this.numberOfSelectedMembers = info.getNumberOfSelectedMembers();
-        this.offeringDetails = info.getOfferingDetails();
-        this.sellingLink = info.getSellingLink();
-        this.applyStartDate = info.getApplyStartDate();
-        this.applyEndDate = info.getApplyEndDate();
-        this.minClouterAge = info.getMinClouterAge();
-        this.maxClouterAge = info.getMaxClouterAge();
-        this.minFollower = info.getMinFollower();
-        this.isEnded = info.getIsEnded();
-        this.companyName = advertiser.getCompanyInfo().getCompanyName();
-        this.companyAvgScore = advertiser.getAvgScore();
-        this.imageInfo = info.getImageList();
+        this.campaign = new CampaignResponse(
+            info.getCampaignId(),
+            info.getAdPlatformList(),
+            info.getPrice(),
+            info.getTitle(),
+            info.getAdCategory(),
+            info.getNumberOfRecruiter(),
+            info.getNumberOfApplicants(),
+            info.getNumberOfSelectedMembers(),
+            info.getRegisterId()
+            );
+        this.advertiserInfo = new AdvertiserResponse(
+            advertiser.getId(),
+            advertiser.getAvgScore(),
+            new CompanyResponse(advertiser.getCompanyInfo().getCompanyName())
+        );
+        this.imageList = info.getImageList();
     }
 
 }
