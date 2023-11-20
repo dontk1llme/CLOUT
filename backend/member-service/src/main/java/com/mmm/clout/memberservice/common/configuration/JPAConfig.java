6a5a1694664e5ab198703a03a3fb50b05f2410f9
정승구 <jsggo2001@naver.com>
2023-11-20 14:39:34 +0900

    Feat: [BE] member-service merge

---
+++ backend/member-service/src/main/java/com/mmm/clout/memberservice/common/configuration/JPAConfig.java
@@ -0,0 +1,23 @@
+package com.mmm.clout.memberservice.common.configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Configuration
+@EnableJpaRepositories(basePackages = "com.mmm.clout.memberservice")
+public class JPAConfig {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory queryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+
+}
