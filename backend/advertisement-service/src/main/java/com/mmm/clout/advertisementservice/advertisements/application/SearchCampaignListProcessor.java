c492ce36e2a8920d263362e2ea829f5a746bf1f2
정승구 <jsggo2001@naver.com>
2023-11-22 15:16:37 +0900

    Refactor: [BE] initialize 부분 패치조인으로 최적화

--- backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/advertisements/application/SearchCampaignListProcessor.java
+++ backend/advertisement-service/src/main/java/com/mmm/clout/advertisementservice/advertisements/application/SearchCampaignListProcessor.java
@@ -12,6 +12,9 @@ import com.mmm.clout.advertisementservice.image.domain.repository.ImageRepositor
 import com.querydsl.jpa.impl.JPAQuery;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
@@ -36,21 +39,46 @@ public class SearchCampaignListProcessor {
         List<Campaign> searchResult = campaignRepository.search(condition, pageable);
         JPAQuery<Campaign> countQuery = campaignRepository.getSearchCountQuery(condition);
 
+        long start = System.currentTimeMillis();
 
-        // TODO n+1 성능 최적화 필요 & image db 조회 더 개선할 수 있을지 고민해보기
-        List<CampaignReader> content = new ArrayList<>();
-        for (Campaign campaign : searchResult) {
-            List<Image> images = imageRepository.findByCampaignId(campaign.getId());
-            AdvertiseSign sign = advertiseSignRepository.findByAdvertisementId(campaign.getId());
-            content.add(
-                new CampaignReader(
-                    campaign.initialize(),
-                    memberProvider.getAdvertiserInfoByMemberId(campaign.getAdvertiserId()),
-                    images,
-                    sign
-                )
-            );
-        }
+        List<Long> idList = searchResult.stream().map(v -> v.getId()).collect(Collectors.toList());
+
+        List<Image> imageList = imageRepository.findByCampaignIdIn(idList);
+        Map<Long, List<Image>> imageMap = imageList.stream()
+            .collect(Collectors.groupingBy(image -> image.getCampaign().getId()));
+
+        List<AdvertiseSign> signList = advertiseSignRepository.findByCampaignIdIn(idList);
+        Map<Long, AdvertiseSign> signMap = signList.stream().collect(Collectors.toMap(
+            sign -> sign.getCampaign().getId(),
+            sign -> sign));
+
+        List<CampaignReader> content = searchResult.stream().map(
+            campaign -> new CampaignReader(
+                campaign,
+                memberProvider.getAdvertiserInfoByMemberId(campaign.getAdvertiserId()),
+                imageMap.get(campaign.getId()),
+                signMap.get(campaign.getId())
+            )
+        ).collect(Collectors.toList());
+
+//        // TODO n+1 성능 최적화 필요 & image db 조회 더 개선할 수 있을지 고민해보기
+//        List<CampaignReader> content = new ArrayList<>();
+//        for (Campaign campaign : searchResult) {
+//            List<Image> images = imageRepository.findByCampaignId(campaign.getId());
+//            AdvertiseSign sign = advertiseSignRepository.findByAdvertisementId(campaign.getId());
+//            content.add(
+//                new CampaignReader(
+//                    campaign,
+//                    memberProvider.getAdvertiserInfoByMemberId(campaign.getAdvertiserId()),
+//                    images,
+//                    sign
+//                )
+//            );
+//        }
+
+        long end = System.currentTimeMillis();
+
+        log.info("time : {}", end - start);
 
         return PageableExecutionUtils.getPage(content, pageable, countQuery::fetchCount);
     }
